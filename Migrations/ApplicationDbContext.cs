2359806b309cd7cb534d80e45750fb42808bae8a
Alexandru Constantin <alexandru.constantin@gmail.com>
2021-07-21 23:32:09 -0400

    Add project files.

---
+++ Migrations/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using EShop.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShop.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public virtual DbSet<Vendor> Vendors { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.Models
+{
+    [Table("Product")]
+    public class Product
+
+    {
+         
+     
+           [Key]
+            public int ProductId { get; set; }
+
+            public string Name { get; set; }
+            public string Code { get; set; }
+
+            public string Description { get; set; }
+
+            public float Price { get; set; }
+
+            public string BarCode { get; set; }
+        
+            public string Model { get; set; }
+
+
+            //public int VendorId { get; set; }
+            //public Vendor Vendor { get; set; }
+
+
+    }
+}
